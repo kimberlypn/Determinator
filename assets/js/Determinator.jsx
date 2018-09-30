b445016f7dcbbabbb221dac04ac6bb01e2090768
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-09-30 04:15:09 -0400

    Designed the home page. Added Login and Registration components but did not add functionality for their corresponding buttons

--- assets/js/Determinator.jsx
+++ assets/js/Determinator.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import {Row, Col, Button} from 'reactstrap';
 
 export default function run(root) {
   ReactDOM.render(<Determinator/>, root);
@@ -8,7 +9,35 @@ export default function run(root) {
 class Determinator extends React.Component {
   render() {
     return (
-      <h2>THE DETERMINATOR</h2>
+      <Fragment>
+        <div id='header'>
+          <Row>
+            <Col md='2'></Col>
+            <Col md='8' className='fade-in' id='brand'>
+              <h1>THE DETERMINATOR</h1>
+            </Col>
+            <Col md='2'></Col>
+          </Row>
+          <Row>
+            <Col md='8'></Col>
+            <Col md='2' id='sub-text'>
+              <p>(yes, it's a word.)</p>
+            </Col>
+            <Col md='2'></Col>
+          </Row>
+          <Row id='account-links'>
+            <Col md='5' className='fade-in' id='login-btn'>
+              <Button>LOGIN</Button>
+            </Col>
+            <Col md='2' className='fade-in' id='gavel'>
+              <i className='fas fa-gavel'></i>
+            </Col>
+            <Col md='5' className='fade-in' id='register-btn'>
+              <Button>REGISTER</Button>
+            </Col>
+          </Row>
+        </div>
+      </Fragment>
     );
   }
 }
